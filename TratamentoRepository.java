94be7a73ce61339e43c0c2a5b252087baae1e4e2
Afonso <145401457+afonsohduarte@users.noreply.github.com>
2025-01-08 17:37:11 +0000

    Create TratamentoRepository.java

---
+++ TratamentoRepository.java
@@ -0,0 +1,7 @@
+package com.example.controle.repository;
+
+import com.example.controle.model.Tratamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TratamentoRepository extends JpaRepository<Tratamento, Long> {
+}
