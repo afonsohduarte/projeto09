27eb48a55bb2e55f9fca711079cf98f7996d95b8
Afonso <145401457+afonsohduarte@users.noreply.github.com>
2025-01-08 17:41:10 +0000

    Update Consulta.java

--- Consulta.java
+++ Consulta.java
@@ -18,4 +18,56 @@ public class Consulta {
     private String mensagem;
 
     // Getters e Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    // Método toString (opcional, ajuda no debug)
+    @Override
+    public String toString() {
+        return "Consulta{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", mensagem='" + mensagem + '\'' +
+                '}';
+    }
 }
