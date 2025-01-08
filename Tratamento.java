73d0d5c3b6fcecf9315f31b650b1fc039d35c7cf
Afonso <145401457+afonsohduarte@users.noreply.github.com>
2025-01-08 17:40:46 +0000

    Update Tratamento.java

--- Tratamento.java
+++ Tratamento.java
@@ -17,4 +17,47 @@ public class Tratamento {
     private String imagemUrl;
 
     // Getters e Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getImagemUrl() {
+        return imagemUrl;
+    }
+
+    public void setImagemUrl(String imagemUrl) {
+        this.imagemUrl = imagemUrl;
+    }
+
+    // Método toString (opcional, ajuda no debug)
+    @Override
+    public String toString() {
+        return "Tratamento{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", imagemUrl='" + imagemUrl + '\'' +
+                '}';
+    }
 }
